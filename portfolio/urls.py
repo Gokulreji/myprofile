4f60558067f74da3c84fce7b5e7d94c5a069705c
gokul reji <gokulreji12@gmail.com>
2021-08-05 14:50:51 +0530

    url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.profile),
+    path('home', views.profile),
     path('',include('myprofile.urls'))
 ]
