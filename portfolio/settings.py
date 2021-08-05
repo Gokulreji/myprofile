34af1f034f562e28e4da3549f8b2f150c2dc3187
gokul reji <gokulreji12@gmail.com>
2021-08-05 12:31:15 +0530

    debug

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dh_g4v3k0_n&)!-0-=jm1ls2(h@4c@p+qhbt%pufvemw+)s8na'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
